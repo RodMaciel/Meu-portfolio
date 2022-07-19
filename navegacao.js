5cfcd9203179b6e03a293155ea4b6142032d81e1
Rodrigo Maciel <rodrigodmc9@gmail.com>
2022-07-19 14:42:33 -0300

    Site portfolio

---
+++ navegacao.js
@@ -0,0 +1,6 @@
+$('#design.card').click(function () {
+    window.location = "http://pt.stackoverflow.com/";
+});
+$('#reforma.card').click(function () {
+    window.location = "http://google.com/";
+});
