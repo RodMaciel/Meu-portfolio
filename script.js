d469ac9ccf8f1f36e7ef19f794b3223ff2773f59
Rodrigo Maciel <rodrigodmc9@gmail.com>
2022-07-25 21:25:41 -0300

    hobbies adicionado

--- script.js
+++ script.js
@@ -5,7 +5,15 @@ $(document).ready(function(){
         }else{
             $('.navbar').removeClass("sticky");
         }
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
+        }
 });
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
 
  $('.menu-btn').click(function(){
     $('.navbar .menu').toggleClass("active");
