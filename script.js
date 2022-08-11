024858c7d556666cd9b0833b04dbbf8cfc6f4e28
Rodrigo Maciel <rodrigodmc9@gmail.com>
2022-08-11 12:38:37 -0300

    animações scroll adicionadas

--- script.js
+++ script.js
@@ -154,3 +154,40 @@ Olá, Gostei do seu portfólio e gostaria de te fazer uma proposta!
 
 
 });
+//debounce melhorias
+const debounce = function(func, wait, immediate) {
+  let timeout;
+  return function(...args) {
+    const context = this;
+    const later = function () {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    const callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+};
+// animacao scroll
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+
+function animeScroll(){
+    const windowTop = window.pageYOffset + (window.innerHeight * 0.75);
+    target.forEach(function (element){
+        if((windowTop) > element.offsetTop){
+            element.classList.add(animationClass);
+        }
+        else {
+            element.classList.remove(animationClass);
+        }
+    })
+}
+animeScroll();
+if(target.length){
+window.addEventListener('scroll',debounce(function(){
+    animeScroll();
+    console.log('teste')
+},200));
+}
