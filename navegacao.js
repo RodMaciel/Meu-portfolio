b968af30a94caea2947b3213498af29acb57c87b
Rodrigo Maciel <rodrigodmc9@gmail.com>
2023-07-05 06:31:35 -0300

    arrumado bug no carrossel

--- navegacao.js
+++ navegacao.js
@@ -1,19 +1,18 @@
-$('#design.card').click(function () {
-    window.open("https://genimaciel.netlify.app/", "_blank");
-
+$(document).on("click", "#design.card", function () {
+  window.open("https://genimaciel.netlify.app/", "_blank");
 });
-$('#reyna.card').click(function () {
-    window.open('https://google.com', '_blank');
+// $('#reyna.card').click(function () {
+//     window.open('https://google.com', '_blank');
 
-});
-$('#jett.card').click(function () {
-    window.open('https://facebook.com', '_blank');
+// });
+// $('#jett.card').click(function () {
+//     window.open('https://facebook.com', '_blank');
 
+// });
+$(document).on("click", "#login.card", function () {
+  window.open("assets/Proj tela login/index.html", "_blank");
 });
-$('#login.card').click(function () {
-    window.open('assets/Proj tela login/index.html', '_blank');
 
-});
-$("#squad.card").click(function () {
+$(document).on("click", "#squad.card", function () {
   window.open("https://squadteam.netlify.app/", "_blank");
 });
