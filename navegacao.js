f92d7d9c9f43b96438f1dd4ced85090adb4e1356
Rodrigo Maciel <rodrigodmc9@gmail.com>
2023-07-05 06:32:13 -0300

    removido comentarios inuteis

--- navegacao.js
+++ navegacao.js
@@ -1,14 +1,6 @@
 $(document).on("click", "#design.card", function () {
   window.open("https://genimaciel.netlify.app/", "_blank");
 });
-// $('#reyna.card').click(function () {
-//     window.open('https://google.com', '_blank');
-
-// });
-// $('#jett.card').click(function () {
-//     window.open('https://facebook.com', '_blank');
-
-// });
 $(document).on("click", "#login.card", function () {
   window.open("assets/Proj tela login/index.html", "_blank");
 });
